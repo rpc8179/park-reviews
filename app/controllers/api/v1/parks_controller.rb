e22642046ca1f541b4833e85fda379fa8bd495db
Andrew <Mclellan.an@gmail.com>
2018-07-30 11:49:10 -0400

    add edit and destroy to parks

--- app/controllers/api/v1/parks_controller.rb
+++ app/controllers/api/v1/parks_controller.rb
@@ -14,13 +14,9 @@ class Api::V1::ParksController < ApplicationController
         if park.save
             render json: { park: park, error: [], successStatus: "Submitted Successfully!"}
         else
-<<<<<<< HEAD
-            render json: { park: {}, error: park.errors.full_messages, works: "" }
+            render json: { park: {}, error: park.errors.full_messages, successStatus: "" }
         end
     end
-=======
-            render json: { park: {}, error: park.errors.full_messages, successStatus: "" }
->>>>>>> a05aa931a38462775ff642ba7784dc1d9add9246
 
     def edit
     end
