e7e49aade103a9aeb41251bf5c79cf0bf84e39d9
Matthew Clarke <MattClarkeRuns@gmail.com>
2018-08-03 14:13:36 -0400

    Update seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,14 @@
 User.create!(
-  email: 'user1@user.cuser',
+  email: 'admin@admin.admin',
+  password: 'aaaaaaa',
+  role: 'admin'
+)
+User.create!(
+  email: 'user@user.user',
   password: 'aaaaaaa'
 )
 User.create!(
-  email: 'user2@user.cuser',
-  password: 'aaaaaaa'
-)
-User.create!(
-  email: 'user3@user.cuser',
+  email: 'bob@ross.paintings',
   password: 'aaaaaaa'
 )
 
@@ -17,39 +18,39 @@ Park.create!(
   city: "Boston",
   state: "MA",
   zip: "02134",
-  description: "Send it to Zoom!  There is a carousel!!"
+  description: "There is a carousel!!"
 )
 Park.create!(
-  name: "Public Garden",
-  address: "139 Tremont Street",
-  city: "Boston",
-  state: "PA",
-  zip: "02134",
-  description: "Send it to Philly!!  There is a cheesesteak!!"
+  name: "Acadia",
+  address: "20 McFarlane Hill Dr.",
+  city: "Bar Harbor",
+  state: "ME",
+  zip: "00345",
+  description: "This is a park that we googled like 45 minutes before we had to present"
 )
 Park.create!(
   name: "Yellowstone",
-  address: "139 Tremont Street",
-  city: "Boston",
-  state: "NJ",
-  zip: "02134",
-  description: "Send it to Atlantic City!  There is a shore!!"
+  address: "Center Loop Road (All of it)",
+  city: "Yellowstone County",
+  state: "MT",
+  zip: "82190",
+  description: "Geysers wow! and other stuff as well!"
 )
 
 Review.create!(
   rating: 5,
-  body: 'Great for dog jumping',
+  body: 'What a wonderful park!',
   user_id: 1,
   park_id: 1
 )
 Review.create!(
   rating: 1,
-  body: 'Not great for dog jumping',
+  body: 'I lost my dog here. :C',
   user_id: 1,
   park_id: 2
 )
 Review.create!(
-  rating: 0,
+  rating: 4,
   body: 'What a delight to the eyes, Trees everywhere, Ahhhhh~',
   park_id: 1,
   user_id: 1
@@ -68,5 +69,10 @@ ReviewUpvote.create!(
 ReviewUpvote.create!(
   user_id: 3,
   review_id: 1,
+  value: -1,
+)
+ReviewUpvote.create!(
+  user_id: 3,
+  review_id: 2,
   value: -1,
 )
